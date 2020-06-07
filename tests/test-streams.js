57b00e81f5739097ce3b7e2ebf7cde94d395bdeb
sabaroof <sabadachmilosz@gmail.com>
2020-06-07 21:01:47 +0200

    test

---
+++ tests/test-streams.js
@@ -0,0 +1,68 @@
+/**
+ * Create test stream
+ * @param {string} url
+ * @param {string} description
+ * @param {boolean} [live]
+ * @param {boolean} [abr]
+ * @param {string[]} [blacklist_ua]
+ * @returns {{url: string, description: string, live: boolean, abr: boolean, blacklist_ua: string[]}}
+ */
+function createTestStream (url, description, live = false, abr = true, blacklist_ua = []) {
+  return {
+    url,
+    description,
+    live,
+    abr,
+    blacklist_ua
+  };
+}
+
+/**
+ * @param {Object} target
+ * @param {Object} [config]
+ * @returns {{url: string, description: string, live: boolean, abr: boolean, blacklist_ua: string[]}}
+ */
+function createTestStreamWithConfig (target, config) {
+  if (typeof target !== 'object') {
+    throw new Error('target should be object');
+  }
+
+  const testStream = createTestStream(target.url, target.description, target.live, target.abr, target.blacklist_ua);
+
+  testStream.config = config;
+
+  return testStream;
+}
+
+module.exports = {
+  bbb: createTestStreamWithConfig({
+    url: 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
+    description: 'Big Buck Bunny - adaptive qualities'
+  },
+  {
+    // try to workaround test failing because of slow seek on Chrome/Win10
+    nudgeMaxRetry: 5
+  }
+  ),
+  borat: {
+  	url: 'https://github.com/sabaroof/PIPIM/tree/master/hls.js/tests/borat_hsl_playlist.m3u8',
+  	description: 'Borat mówi dziękuje',
+  	live: false,
+  	abr: false,
+  	blacklist_ua: [],
+  },
+  LabScreenCap1: {
+  	url: 'https://github.com/sabaroof/PIPIM/tree/master/hls.js/tests/hsl_playlist1.m3u8',
+  	description: 'Pierwszy screencap z projektu',
+  	live: false,
+  	abr: false,
+  	blacklist_ua: [],
+  	},
+  LabScreenCap2: {
+  	url: 'https://github.com/sabaroof/PIPIM/tree/master/hls.js/tests/hsl_playlist2.m3u8',
+  	description: 'Drugi screencap z projektu',
+  	live: false,
+  	abr: false,
+  	blacklist_ua: [],
+  },
+};
---
+++ tests/unit/controller/eme-controller.js
@@ -0,0 +1,168 @@
+import EMEController from '../../../src/controller/eme-controller';
+import HlsMock from '../../mocks/hls.mock';
+import { EventEmitter } from 'eventemitter3';
+import { ErrorDetails } from '../../../src/errors';
+
+const sinon = require('sinon');
+
+const MediaMock = function () {
+  let media = new EventEmitter();
+  media.setMediaKeys = sinon.spy();
+  media.addEventListener = media.addListener.bind(media);
+  media.removeEventListener = media.removeListener.bind(media);
+  return media;
+};
+
+const fakeLevels = [
+  {
+    audioCodec: 'audio/foo'
+  },
+  {
+    videoCoded: 'video/foo'
+  }
+];
+
+let emeController;
+let media;
+
+const setupEach = function (config) {
+  media = new MediaMock();
+
+  emeController = new EMEController(new HlsMock(config));
+};
+
+describe('EMEController', function () {
+  beforeEach(function () {
+    setupEach();
+  });
+
+  it('should not do anything when `emeEnabled` is false (default)', function () {
+    let reqMediaKsAccessSpy = sinon.spy();
+
+    setupEach({
+      requestMediaKeySystemAccessFunc: reqMediaKsAccessSpy
+    });
+
+    emeController.onMediaAttached({ media });
+    emeController.onManifestParsed({ media });
+
+    expect(media.setMediaKeys.callCount).to.equal(0);
+    expect(reqMediaKsAccessSpy.callCount).to.equal(0);
+  });
+
+  it('should request keys when `emeEnabled` is true (but not set them)', function (done) {
+    let reqMediaKsAccessSpy = sinon.spy(function () {
+      return Promise.resolve({
+        // Media-keys mock
+      });
+    });
+
+    setupEach({
+      emeEnabled: true,
+      requestMediaKeySystemAccessFunc: reqMediaKsAccessSpy
+    });
+
+    emeController.onMediaAttached({ media });
+
+    expect(media.setMediaKeys.callCount).to.equal(0);
+    expect(reqMediaKsAccessSpy.callCount).to.equal(0);
+
+    emeController.onManifestParsed({ levels: fakeLevels });
+
+    setTimeout(function () {
+      expect(media.setMediaKeys.callCount).to.equal(0);
+      expect(reqMediaKsAccessSpy.callCount).to.equal(1);
+      done();
+    }, 0);
+  });
+
+  it('should request keys with specified robustness options when `emeEnabled` is true', function (done) {
+    let reqMediaKsAccessSpy = sinon.spy(function () {
+      return Promise.resolve({
+        // Media-keys mock
+      });
+    });
+
+    setupEach({
+      emeEnabled: true,
+      drmSystemOptions: {
+        audioRobustness: 'HW_SECURE_ALL',
+        videoRobustness: 'HW_SECURE_ALL'
+      },
+      requestMediaKeySystemAccessFunc: reqMediaKsAccessSpy
+    });
+
+    emeController.onMediaAttached({ media });
+
+    expect(media.setMediaKeys.callCount).to.equal(0);
+    expect(reqMediaKsAccessSpy.callCount).to.equal(0);
+
+    emeController.onManifestParsed({ levels: fakeLevels });
+
+    setTimeout(function () {
+      const baseConfig = reqMediaKsAccessSpy.getCall(0).args[1][0];
+      expect(baseConfig.audioCapabilities[0]).to.have.property('robustness', 'HW_SECURE_ALL');
+      expect(baseConfig.videoCapabilities[0]).to.have.property('robustness', 'HW_SECURE_ALL');
+      done();
+    }, 0);
+  });
+
+  it('should trigger key system error(s) when bad encrypted data is received', function (done) {
+    let reqMediaKsAccessSpy = sinon.spy(function () {
+      return Promise.resolve({
+        // Media-keys mock
+      });
+    });
+
+    setupEach({
+      emeEnabled: true,
+      requestMediaKeySystemAccessFunc: reqMediaKsAccessSpy
+    });
+
+    let badData = {
+      initDataType: 'cenc',
+      initData: 'bad data'
+    };
+
+    emeController.onMediaAttached({ media });
+    emeController.onManifestParsed({ levels: fakeLevels });
+
+    media.emit('encrypted', badData);
+
+    setTimeout(function () {
+      expect(emeController.hls.trigger).to.have.been.calledTwice;
+      expect(emeController.hls.trigger.args[0][1].details).to.equal(ErrorDetails.KEY_SYSTEM_NO_KEYS);
+      expect(emeController.hls.trigger.args[1][1].details).to.equal(ErrorDetails.KEY_SYSTEM_NO_SESSION);
+      done();
+    }, 0);
+  });
+
+  it('should close all media key sessions and remove media keys when media is detached', function (done) {
+    let reqMediaKsAccessSpy = sinon.spy(function () {
+      return Promise.resolve({
+        // Media-keys mock
+      });
+    });
+    let keySessionCloseSpy = sinon.spy(() => Promise.resolve());
+
+    setupEach({
+      emeEnabled: true,
+      requestMediaKeySystemAccessFunc: reqMediaKsAccessSpy
+    });
+
+    emeController.onMediaAttached({ media });
+    emeController._mediaKeysList = [{
+      mediaKeysSession: {
+        close: keySessionCloseSpy
+      }
+    }];
+    emeController.onMediaDetached();
+
+    setTimeout(function () {
+      expect(keySessionCloseSpy.callCount).to.equal(1);
+      expect(emeController._mediaKeysList.length).to.equal(0);
+      expect(media.setMediaKeys.calledWith(null)).to.be.true;
+      done();
+    }, 0);
+  });
+});
